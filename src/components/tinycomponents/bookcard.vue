b175f071216b644bc46d4f0d1e4dd5cb793cfb66
zentialEdwardSu <zentialeds@gmail.com>
2022-11-12 19:40:32 +0800

    Add content and fix a little style bug

--- src/components/tinycomponents/bookcard.vue
+++ src/components/tinycomponents/bookcard.vue
@@ -1,7 +1,9 @@
 <template>
     <el-card class="bc-cardmain" body-style="{padding:0px}" shadow="never">
         <img class="bc-img" :src="bookinfo.src"/>
-        <el-button color="black" type="text" class="bc-content">{{bookinfo.text}}</el-button>
+        <div class="bc-content">
+            <el-button color="black" type="text">{{bookinfo.text}}</el-button>
+        </div>
     </el-card>
 </template>
 <script>
@@ -19,8 +21,8 @@ export default{
     width: 45%;
     height: 200px;
     display: flex;
-    justify-content: left;
-    flex-wrap: wrap;
+    justify-content: space-between;
+    flex-wrap:nowrap;
     margin:  15px;
     margin-top: 0px;
     background-color: transparent;
@@ -37,8 +39,9 @@ export default{
 }
 .bc-content{
     /* color: black; */
-    font-size: x-large;
+    font-size:small;
     font-weight: bold;
+    word-wrap: break-word;
 }
 
 </style>
